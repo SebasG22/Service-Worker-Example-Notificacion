46bfd918e9c49a32ffea0d4cdc02d57f91018b3c
Sebastián Guevara <rlxsebas@gmail.com>
2018-01-08 07:57:52 -0500

    Notify user when new version is available

---
+++ sw.js
@@ -0,0 +1,18 @@
+var CACHE_NAME = 'my-site-cache-v4';
+var urlsToCache = ['/','index.html', '/styles.css'];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
